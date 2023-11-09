92f04cfa16e09f4a80fc4e9ef34a315bc4806316
PShortt07 <144266245+PShortt07@users.noreply.github.com>
2023-11-09 13:31:48 +0000

    Update Program.cs

--- homework/Program.cs
+++ homework/Program.cs
@@ -10,6 +10,7 @@ namespace homework
 {
     internal class Program
     {
+        //Creates a new structure to hold all information about a piece of homework
         struct homework
         {
             public string subject;
@@ -17,9 +18,14 @@ namespace homework
             public DateTime date;
             public bool complete;
         }
+        //Displays the menu on the console
         static void DisplayMenu()
         {
+            Console.Clear();
             int x = 0;
+            //Creates a new list containing all homework already on the file
+            List<homework> homeworkList = new List<homework>();
+            homeworkList = listGenerator(homeworkList);
             while (x == 0)
             {
                 Console.WriteLine("1. View Homework\n2. Add Homework\n3. Complete Homework\n4. Quit");
@@ -35,13 +41,23 @@ namespace homework
                 switch (x)
                 {
                     case 1:
-                        DisplayHomework();
+                        DisplayHomework(homeworkList);
+                        Console.ReadLine();
                         break;
                     case 2:
-                        AddHomework();
+                        //Checks if the limit of 20 has been reached
+                        if (homeworkList.Count < 20)
+                        {
+                            AddHomework();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Maximum of 20 pieces of homework reached.");
+                            Console.ReadLine();
+                        }
                         break;
                     case 3:
-                        CompleteHomework();
+                        CompleteHomework(homeworkList);
                         break;
                     case 4:
                         Quit();
@@ -53,14 +69,15 @@ namespace homework
                 }
             }
         }
-        static void DisplayHomework()
+        static List<homework> listGenerator(List<homework> homeworkList)
         {
+            //Opens file to be read
             StreamReader fileReader = new StreamReader("homework.txt");
-            List<homework> homeworkList = new List<homework>();
             int x = 0;
-            homework homework = new homework();
+            //Creates a new 'homework' variable to be used to take in information from the file and put it into the list
             while (!fileReader.EndOfStream)
             {
+                homework homework = new homework();
                 if (x == 2)
                 {
                     homework.date = DateTime.Parse(fileReader.ReadLine());
@@ -76,14 +93,54 @@ namespace homework
                     x++;
                 }
                 x++;
-                if (x == 4)
+                //Only adds if the homework is due in the future
+                if (x == 4 && homework.date > DateTime.Today)
                 {
+                    //Adds to list
                     homeworkList.Add(homework);
-                    homework = new homework();
                     x = 0;
                 }
             }
+            fileReader.Close();
+            return homeworkList;
         }
+        //Displays all homework
+        static void DisplayHomework(List<homework> homeworkList)
+        {
+            int x = 1;
+            foreach (homework i in homeworkList)
+            {
+                //Displays in red if it is due in less than 3 days and incomplete
+                if (i.date.Day - DateTime.Today.Day <= 3 && i.complete == false)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+                else if (i.complete == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.Write($"{x}. ");
+                Console.WriteLine(i.subject);
+                Console.WriteLine(i.desc);
+                Console.WriteLine(i.date);
+                //Checks if work is complete
+                if (i.complete == true)
+                {
+                    Console.WriteLine("Done");
+                }
+                else
+                {
+                    Console.WriteLine("Not Done");
+                }
+                x++;
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        //Adds a piece of homework to the file
         static void AddHomework()
         {
             StreamWriter fileWriter = new StreamWriter("homework.txt", true);
@@ -110,59 +167,71 @@ namespace homework
                     date = DateTime.Now;
                 }
             } while (date == DateTime.Now);
+            fileWriter.WriteLine(date.ToString());
             fileWriter.WriteLine("false");
+            fileWriter.Close();
         }
-        static void CompleteHomework()
+        //Allows user to mark a piece of homework as complete
+        static void CompleteHomework(List<homework> homeworkList)
         {
-
+            int num = 0;
+            do
+            {
+                //Displays homework
+                DisplayHomework(homeworkList);
+                Console.WriteLine("Enter homework number:");
+                try
+                {
+                    num = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error");
+                    num = 0;
+                }
+                //Checks if the chosen homework exists
+                if (num < 1 || num > homeworkList.Count)
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+                    Console.Clear();
+                    num = 0;
+                }
+            } while (num == 0);
+            //Changes the 'complete' value of the chosen homework in the list to true
+            for (int i = 1; i <= homeworkList.Count; i++)
+            {
+                if (i == num)
+                {
+                    homework x = homeworkList[i-1];
+                    x.complete = true;
+                    homeworkList[i-1] = x;
+                }
+            }
+            //Writes the new value to the file
+            StreamWriter fileWriter = new StreamWriter("homework.txt");
+            foreach (homework homework in homeworkList)
+            {
+                fileWriter.WriteLine(homework.subject.ToString());
+                fileWriter.WriteLine(homework.desc.ToString());
+                fileWriter.WriteLine(homework.date.ToString());
+                fileWriter.WriteLine(homework.complete.ToString());
+            }
+            fileWriter.Close();
         }
+        //Exits the program
         static void Quit()
         {
             Environment.Exit(0);
         }
         static void Main(string[] args)
         {
-            DisplayMenu();
-            //while (!fileReader.EndOfStream)
+            //Keeps the menu in a loop
+            int x = 0;
+            while (x == 0)
             {
-                //if (x % 2 == 0)
-                {
-                    //DateTime date = DateTime.Parse(fileReader.ReadLine());
-                    //if (date < DateTime.Today)
-                    {
-                        //StreamWriter fileWriter = new StreamWriter("homework.txt", true);
-                    }
-                    //else
-                    {
-                        //if (date.Day - DateTime.Today.Day <= 3)
-                        {
-                            //Console.BackgroundColor = ConsoleColor.Red;
-                            //Console.Write(date);
-                            //Console.BackgroundColor = ConsoleColor.Black;
-                        }
-                        //else
-                        {
-                            //Console.Write(date);
-                        }
-                    }
-                }
-                //else if (x % 3 == 0)
-                {
-                    //string workComplete = fileReader.ReadLine();
-                    //if (workComplete == "done")
-                    {
-                        //Console.BackgroundColor = ConsoleColor.Green;
-                        //Console.WriteLine(workComplete);
-                        //Console.BackgroundColor = ConsoleColor.Black;
-                    }
-                }
-                //else
-                {
-                    //Console.Write(fileReader.ReadLine());
-                }
-                //x++;
+                DisplayMenu();
             }
-            //Console.ReadLine();
         }
     }
 }
